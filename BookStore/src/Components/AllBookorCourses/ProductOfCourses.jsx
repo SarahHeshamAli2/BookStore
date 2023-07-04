08649fffbb26f2fc7091ba1072a8d457a11ce120
sarah hesham <etshyko@gmail.com>
2023-07-04 22:17:09 +0300

    new

--- BookStore/src/Components/AllBookorCourses/ProductOfCourses.jsx
+++ BookStore/src/Components/AllBookorCourses/ProductOfCourses.jsx
@@ -6,10 +6,21 @@ import LoadingScreen from "../LoadingScreen/LoadingScreen";
 import './Products.css'
 import { cartContext } from "../Context/CartContext";
 export default function ProductsOfCourses(){
+
+    const [filteredPro , setFilterd] = useState(null)
+
     const {addToCart} = useContext(cartContext)
 
     const [allProducts , setAllProducts]=useState(null);
+function filterProducts() {
+    
 
+    const myNewFilter = allProducts.filter((pro)=> { return pro.isCourseOrBook == "Book"})
+    setFilterd(myNewFilter)
+    console.log(filteredPro);
+    
+
+}
     async function getAllPrducts(){
 
        try{
@@ -32,26 +43,26 @@ export default function ProductsOfCourses(){
       <div className="row">
       <h2 className="text-center my-5">Books At Bookland</h2>
         {allProducts.map(function(product, idx){
-            return <div key={ idx } className="col-md-3 pt-5  mb-5">
-              <div>
+            return product.isCourseOrBook === "Course" ?  <div key={ idx } className="col-md-3 pt-5  mb-5">
+            <div>
 
-            <div className="item">
-                <Link  to= {`/Showbook/${product._id}`}>
-                <img src={product.imageCover.slice(19)} alt={product.productName} />
-                 <h3 className="h6 fw-bolder text-center pt-3">
-                    {product.productName.slice(0,product.productName.indexOf('',30))}</h3>
-                    <p className=" text-center text-muted ">{product.price}$</p>
-                     <span className=" text-center">
-                      <i className="fas fa-star"></i>{product.ratingsAverage}
-                      </span>
-                </Link>
-                      <br/>
-                     <div className="text-center"> 
-                     <div onClick={function(){addToCart(product._id)}} className="btn btnhover btnhover2 text-white" to=""><i className="flaticon-shopping-cart-1  fa-solid fa-cart-shopping text-white"></i>   Add to cart<i/></div>
-                     </div>
-            </div>
-            </div>
-        </div>})}
+          <div className="item">
+              <Link  to= {`/Showbook/${product._id}`}>
+              <img src={product.imageCover.slice(19)} alt={product.productName} />
+               <h3 className="h6 fw-bolder text-center pt-3">
+                  {product.productName.slice(0,product.productName.indexOf('',30))}</h3>
+                  <p className=" text-center text-muted ">{product.price}$</p>
+                   <span className=" text-center">
+                    <i className="fas fa-star"></i>{product.ratingsAverage}
+                    </span>
+              </Link>
+                    <br/>
+                   <div className="text-center"> 
+                   <div onClick={function(){addToCart(product._id)}} className="btn btnhover btnhover2 text-white" to=""><i className="flaticon-shopping-cart-1  fa-solid fa-cart-shopping text-white"></i>   Add to cart<i/></div>
+                   </div>
+          </div>
+          </div>
+      </div> : "" })}
     </div>
 </div> :<LoadingScreen/> } 
         
